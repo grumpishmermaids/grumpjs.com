2e57156eab1b541564d30d9585328952765f73c5
hdh3000 <henry.hutcheson@gmail.com>
2015-10-01 11:59:15 -0400

    added a my grumps route to main app, and included module

--- server/client/app/services/services.js
+++ server/client/app/services/services.js
@@ -1,6 +1,6 @@
 angular.module('grump.services', [])
 
-.factory('Files', function ($http) {
+.factory('Files', function ($http, $window) {
   // Your code here
   var submitGrump = function (obj) {
     return $http({
@@ -23,22 +23,71 @@ angular.module('grump.services', [])
 
   return {
     submitGrump : submitGrump,
-    getGrumps  : getGrumps
+    getGrumps  : getGrumps,
   };
 
 })
-.factory('Auth', function ($http) {
-  var initiateSignin = function() {
+.factory('MyGrumps', function ($http, $window) {
+  //this is the only set of requests that have to deal with permissions-- so token has to be sent in header
+  var tokenHeader = {
+    //have to include content type else delete wont work.... angular defaults
+    "Content-Type": "application/json;charset=utf-8",
+    'x-access-token': $window.localStorage.getItem('grumpToken') 
+  };
+
+  var getMyGrumps = function () {
     return $http({
       method: 'GET',
-      url: '/api/auth/signin'
-    }).then(function(resp){
+      headers: tokenHeader,
+      url: 'api/mygrumps'
+    }).then(function (resp) {
       return resp;
     });
   };
-  
+
+  var updateGrump = function (grumpID) {
+    return $http({
+      method: 'PUT',
+      headers: tokenHeader,
+      url: 'api/mygrumps',
+      data: {grumpID: grumpID}
+    }).then(function (resp) {
+      return resp;
+    });
+  };
+
+  var deleteGrump = function (grumpID) {
+    return $http({
+      method: 'DELETE',
+      headers: tokenHeader,
+      url: 'api/mygrumps',
+      data: {grumpID: grumpID}
+    }).then(function (resp) {
+      return resp;
+    });
+  };
+
   return {
-    initiateSignin : initiateSignin
+    getMyGrumps  : getMyGrumps,
+    updateGrump  : updateGrump,
+    deleteGrump  : deleteGrump
+  };
+})
+.factory('AuthState', function () {
+  //this factory is only here to keep track of whether or not our user has signed in
+  var authState = false;
+
+  var getAuthState = function(){
+    return authState;
+  };
+
+  var setAuthState = function(newState){
+    authState = newState;
+    return authState;
+  };
+   return {
+    getAuthState  : getAuthState,
+    setAuthState  : setAuthState
   };
 
 });
