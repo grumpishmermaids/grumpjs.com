f607ca79c9250e7b5eb7d85769bed8f00260cb49
hdh3000 <henry.hutcheson@gmail.com>
2015-10-01 12:09:11 -0400

    wrote some drafty specs for auth server - need to make better

--- specs/server/authRouterSpec.js
+++ specs/server/authRouterSpec.js
@@ -1,34 +1,47 @@
-var expect   = require('chai').expect;
-var assert   = require('chai').assert;
-var request  = require('supertest');
-var mongoose = require('mongoose');
+var expect    = require('chai').expect;
+var assert    = require('chai').assert;
+var sinon     = require('sinon');
+var supertest = require('supertest');
+var mongoose  = require('mongoose');
 var GitHubApi = require('github');
+var router    = require('express').Router();
+var http      = require('http');
+var request   = require('request');
 
 
-describe('api/auth', function () {
+// describe('api/auth', function () {
 
+//   describe('GET at /signin', function () {
+//     beforeEach(function(){
+//         //mocking out are request function
+//         sinon.stub(request, 'post', function(options, cb) {
+//             var resp = {access_token:'tokinToken', scope: "user:email"};
+//             var err  = null;
+//             cb(err, resp);
+//         });
 
-  describe('/signin', function () {
-    // not sure how to test api and dont want to get hung on it while i could be
-    // doing other things
-    // it('should send back a header with an access token', function (done) {
-    //   request('http://localhost:3000/api/auth/signin')
-    //     .get('/')
-    //     .expect(200)
-    //     .end(function (err, res) {
-    //       if (err) return done(err);
-    //       console.log(res);
-    //       expect(res.header.location).to.eql('/');
-    //       done();
-    //     });
-    // });
+//         sinon.stub(router, 'get', function(arg, cb) {
+//             var req = arg;
+//             var res = {};
+//             cb(req,res);
+//         });
+//     });
 
-    // it('should redirect the user to the base route', function () {
+//     afterEach(function () {
+//         request.post.restore();
+//     });
+
+//     it('should build the correct options object', function (done) {
+//         supertest('http://localhost:3000')
+//         .get('/signin?code=ghostofbarryblaha')
+//         .end(function(req, res){
+//             console.log(req);
+//             done();
+//         });
     
-    // });
+//     });
+
+//   });
 
 
-  });
-
-
-});
+// });
