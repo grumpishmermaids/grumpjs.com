da44c14e732f5f60ec246d32dc8a56231397b900
hdh3000 <henry.hutcheson@gmail.com>
2015-10-01 11:56:26 -0400

    set up an auth controller that passes user through github signin

--- server/client/app/auth/auth.js
+++ server/client/app/auth/auth.js
@@ -1,6 +1,6 @@
 angular.module('grump.auth', [])
 
-.controller('AuthController', function ($window, $scope, Auth) {
+.controller('AuthController', function ($window, $scope) {
   //this sends the user to the github signin page
   $scope.login=function() {
       var scope = "user:email"; // this sets meta-data we are able to access about user through github api
